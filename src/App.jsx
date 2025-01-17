033b70ae2ce3afcbc5ceb3f2dad3b580b502ed1f
Luca <lucan.personal@gmail.com>
2025-01-17 23:46:43 +1100

    ensured average userRating is fixed to 2.d.p

--- src/App.jsx
+++ src/App.jsx
@@ -350,7 +350,7 @@ function WatchedSummary({ watched }) {
         </p>
         <p>
           <span>🌟</span>
-          <span>{avgUserRating}</span>
+          <span>{avgUserRatin.toFixed(2)}</span>
         </p>
         <p>
           <span>⏳</span>
